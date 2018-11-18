b088f7a0fd2464e109defd5bd3e4a37bc17cac15
Roni Juppi <roni.juppi@gmail.com>
2018-11-18 23:03:59 +0200

    First commit. We parse the jamix website for the lunch infos and such

---
+++ index.js
@@ -0,0 +1,50 @@
+const puppeteer = require('puppeteer')
+
+function wait(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}
+
+const parseFood = () => {
+  // Finds separate "squares" inside the page, containing separate lunch menus, such as "Lounas", "Kasvislounas", etc.
+  return Array.from(document.querySelectorAll('.v-button-multiline'))
+    .map((element) => {
+      return {
+        // This finds the title inside the element
+        title: element.querySelector('.multiline-button-caption-text').textContent,
+        // This finds each separate lunch item inside the element 
+        contents: Array.from(element.querySelectorAll('.multiline-button-content-text')).map(x => x.textContent)
+      }
+    })
+}
+
+(async () => {
+  const browser = await puppeteer.launch()
+  
+  const page = await browser.newPage()
+  await page.goto('https://www.jamix.fi/ruokalistat/?anro=97440&k=50&mt=4', {waitUntil: 'networkidle2'})
+  
+  // Clicking on next page button to get a weekday
+  await page.evaluate(() => {
+    document.querySelector('.v-button-date--next').click()
+  })
+
+  // When we find this element, we can assume the page is loaded.
+  await page.waitForSelector('.multiline-button-caption-text')
+
+  const contents1and2 = await page.evaluate(parseFood)
+  // Clicks on the "Linjasto 3" button
+  await page.evaluate(() => {
+    document.querySelector('.v-tabsheet-tabitemcell-first .v-captiontext').click()
+  })
+  await wait(1000)
+  const contents3 = await page.evaluate(parseFood)
+  
+  //await page.screenshot({path: 'example.png'})
+  
+  console.log(contents1and2)
+  console.log(contents3)
+  
+  await browser.close()
+})()
